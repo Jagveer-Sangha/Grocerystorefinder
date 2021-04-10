bb2c6642369d3534a17e5fbc449354ca81a0a2d5
Jagveer Sangha <jagveersangha@gmail.com>
2021-04-10 00:07:39 -0400

    sad

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-var map;
-require(["esri/map"], function (Map) {
-    map = new Map("map", {
-        basemap: "streets-navigation-vector",
-        center: [-79.3832, 43.6532],
-        zoom: 13
+// var map;
+// require(["esri/map"], function (Map) {
+//     map = new Map("map", {
+//         basemap: "streets-navigation-vector",
+//         center: [-79.3832, 43.6532],
+//         zoom: 13
+//     });
+// });
+
+// var search = new Search({
+//     map: map
+// }, "search");
+// search.startup();
+
+require([
+    "esri/map",
+    "esri/dijit/Search",
+    "esri/geometry/Extent",
+    "esri/graphic",
+    "esri/symbols/SimpleMarkerSymbol",
+    "esri/geometry/screenUtils",
+    "dojo/dom",
+    "dojo/dom-construct",
+    "dojo/query",
+    "dojo/_base/Color"
+], function (Map, Search, Extent, Graphic, SimpleMarkerSymbol, screenUtils, dom, domConstruct, query, Color) {
+    // create a map and instance of the search widget here
+    var map;
+    require(["esri/map"], function (Map) {
+        map = new Map("map", {
+            basemap: "streets-navigation-vector",
+            center: [-79.3832, 43.6532],
+            zoom: 13
+        });
+
+        var search = new Search({
+            map: map,
+        }, dom.byId("search"));
+
+        search.startup();
     });
 });
-
-var search = new Search({
-    map: map
-}, "search");
-search.startup();
