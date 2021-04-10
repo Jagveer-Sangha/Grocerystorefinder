45609ebc2d776147b1235a353f38a803a36d228b
DuckyZero <61894684+DuckyZero@users.noreply.github.com>
2021-04-10 16:32:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ require([
         search.startup();
     });
 });
+
+// Navbar function
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
