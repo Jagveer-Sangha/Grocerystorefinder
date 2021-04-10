acf061035ca2aec85d1382ee4cfd1132d30e5ee7
Jagveer-Sangha <61700208+Jagveer-Sangha@users.noreply.github.com>
2021-04-09 23:32:20 -0400

    Update script.js
    Found the basic map layout that will be used. Started implementing the search bar.

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var map;
 require(["esri/map"], function (Map) {
     map = new Map("map", {
-        basemap: "topo",
-        center: [-122.45, 37.75],
+        basemap: "streets-navigation-vector",
+        center: [-79.3832, 43.6532],
         zoom: 13
     });
 });
+
+var search = new Search({
+    map: map
+}, "search");
+search.startup();
